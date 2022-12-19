580529c62153e783be03e03694acfb556ccd6ccc
Isaac Wilson <iswilson777@gmail.com>
2022-12-19 03:04:40 -0800

    Added 2-strlen.c anf modified main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strlen - returns the length 
+ *of a string
+ *@s : pointer to first character
+ */
+int _strlen(char *s)
+{
+	char nullChar = '\0';
+	
+	int length = 0;
+	
+	int i = 0;
+
+	while (s[i] != nullChar)
+	{
+		length++;
+		i++;
+	}
+	return (length);
+}
+
