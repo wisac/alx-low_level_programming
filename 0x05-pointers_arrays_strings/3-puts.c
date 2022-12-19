0de48708e2f0ffa020598e3f52862305c30f814e
Isaac Wilson <iswilson777@gmail.com>
2022-12-19 06:43:19 -0800

    Added 3-puts.c and modified main.h

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**_puts - prints a string
+ *to stdout
+ *
+ *@s : string to be printed
+ */
+void _puts(char *str)
+{
+	char strEnd = '\0';
+
+	int i = 0;
+
+	while ((str[i]) != strEnd)
+	{
+		_putchar((str[i]));
+		i++;
+	}
+	_putchar('\n');
+}
