0696a5996b6ae94d21b181189bc24f04863460d0
Isaac Wilson <iswilson777@gmail.com>
2023-01-05 20:14:27 +0000

    Added 2-strlen_recursion.c and modified main.h

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - finds the length of a string
+ * @s: pointer to first char in string
+ * Return: the length of a  string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
