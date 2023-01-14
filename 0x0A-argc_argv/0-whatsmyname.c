abd7f471347089aced6e612653400efeeb865a47
Isaac Wilson <iswilson777@gmail.com>
2023-01-14 21:44:11 +0000

    Added 0-whatsmyname.c file and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number arguments passed
+ * @argv: array of strings arguments passed
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
