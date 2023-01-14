b48ad91ee6dc359159c88dcbea62078fc8154b2f
Isaac Wilson <iswilson777@gmail.com>
2023-01-14 22:21:54 +0000

    Added 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number arguments passed
+ * @argv: array of strings arguments passed
+ *
+ * Return: 0 (success)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
+
